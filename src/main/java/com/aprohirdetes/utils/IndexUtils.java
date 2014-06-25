41d774a5f349009c0979ed3efc5f69b01c53a31f
Balazs VAMOS <bvamos@zuriel.net>
2014-06-25 08:59:20 +0200

    Kulcsszavak finomitasa

--- src/main/java/com/aprohirdetes/utils/IndexUtils.java
+++ src/main/java/com/aprohirdetes/utils/IndexUtils.java
@@ -10,11 +10,26 @@ public class IndexUtils {
 	 * List of Hungarian stopwords
 	 */
 	public static ArrayList<String> STOPWORDS_HU = new ArrayList<String>(
-			Arrays.asList("a", "az", "egy", "be", "ki", "le", "fel", "meg",
-					"el", "át", "rá", "ide", "oda", "szét", "össze", "vissza",
-					"de", "hát", "és", "vagy", "hogy", "van", "lesz", "volt",
-					"csak", "nem", "igen", "mint", "én", "te", "ő", "mi", "ti",
-					"ők", "ön", "pl", "stb", "még", "aha"));
+			Arrays.asList("a", "aha", "aki", "ami", "arra", "az", "azt", "át", 
+					"be", 
+					"csak", 
+					"de", 
+					"egy", "el", "erre", "ez", "ezt", "én", "és", 
+					"fel", 
+					"ha", "hát", "hogy", 
+					"ide", "igen", "is", "itt", 
+					"ki", 
+					"le", "lesz",
+					"más", "még", "meg", "mi", "mint", 
+					"nem", 
+					"oda", "össze", "ő", "ők", "ön",   
+					"pl", 
+					"rá", 
+					"stb", "szét", 
+					"te", "ti",
+					"vagy", "van", "vissza", "volt"
+				)
+			);
 
 	/**
 	 * Egy adott szoveg tokenekre bontasat vegzi. 
@@ -33,9 +48,23 @@ public class IndexUtils {
 		for (String s : text.split("\\s")) {
 			String word = s.replaceAll("[\\.,:;'\"\\!\\?]", "").toLowerCase();
 
-			if (!IndexUtils.STOPWORDS_HU.contains(word) && !ret.contains(word)) {
-				ret.add(word);
+			// Ures szavak kiszurese
+			if(word.isEmpty()) {
+				continue;
 			}
+			
+			// Stopword vagy letezo szavak atugrasa
+			if (IndexUtils.STOPWORDS_HU.contains(word) || ret.contains(word)) {
+				continue;
+			}
+			
+			// Szamokat tartalmazo szavak atugrasa
+			if(word.matches(".*\\d.*")){
+				continue;
+			}
+			
+			// Hozzadas a kulcsszavak listajahoz
+			ret.add(word);
 		}
 
 		return ret;
