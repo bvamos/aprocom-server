5c2af0f821000e9ea490b8c2e76cff0f556ca12d
Balazs Vamos <bvamos@zuriel.net>
2015-02-19 14:29:03 +0100

    Task idozites javitasa

--- src/main/java/com/aprohirdetes/server/AproApplication.java
+++ src/main/java/com/aprohirdetes/server/AproApplication.java
@@ -238,10 +238,10 @@ public class AproApplication extends Application {
 		AttributumCache.loadAttributumCache();
 		
 		// Hirdetesek szamanak szamolasa idozitve a hatterben
-		getTaskService().scheduleWithFixedDelay(new KategoriaCountTask(getLogger()), 5, 10, TimeUnit.MINUTES);
+		getTaskService().scheduleWithFixedDelay(new KategoriaCountTask(getLogger()), 5, 600, TimeUnit.SECONDS);
 		
 		// Lejaro hirdetesek feladoinak ertesitese naponta egyszer
-		getTaskService().scheduleWithFixedDelay(new LejaratErtesitoTask(getLogger()), 10, 1, TimeUnit.HOURS);
+		getTaskService().scheduleWithFixedDelay(new LejaratErtesitoTask(getLogger()), 1, 1, TimeUnit.HOURS);
 	}
 
 	@Override
