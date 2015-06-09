6520ae586fe16d6e7fb6fc402304421a94862627
bvamos <bvamos@zuriel.net>
2015-06-09 20:57:14 +0200

    Nyomtatasi kep: torolt hirdetest nem mutatunk

--- src/main/java/com/aprohirdetes/server/HirdetesNyomtatasServerResource.java
+++ src/main/java/com/aprohirdetes/server/HirdetesNyomtatasServerResource.java
@@ -84,69 +84,74 @@ public class HirdetesNyomtatasServerResource extends ServerResource implements
 		
 		// Hirdetes adatai
 		if(hirdetes != null) {
-			// HTML Title modositasa
-			((HashMap<String, Object>) dataModel.get("app")).put("htmlTitle", getApplication().getName() + " - " + hirdetes.getCim());
-			((HashMap<String, Object>) dataModel.get("app")).put("description", hirdetes.getCim());
-			
-			// Mongo Datastore
-			Datastore datastore = MongoUtils.getDatastore();
-			
-			// Megjelenes szamanak novelese
-			Query<Hirdetes> query = datastore.createQuery(Hirdetes.class);
-			query.criteria("id").equal(this.hirdetesId);
-			UpdateOperations<Hirdetes> ops = datastore.createUpdateOperations(Hirdetes.class).set("megjelenes", hirdetes.getMegjelenes()+1);
-			datastore.update(query, ops);
-		    hirdetes.increaseMegjelenesByOne();
-		    
-			
-			// Kereses eredmenyeben levo Hirdetes objektum feltoltese kepekkel, egyeb adatokkal a megjeleniteshez
-			hirdetes.getEgyebMezok().put("tipusNev", (hirdetes.getTipus()==HirdetesTipus.KINAL) ? "Kínál" : "Keres");
-			
-			Kategoria kat = KategoriaCache.getCacheById().get(hirdetes.getKategoriaId());
-			hirdetes.getEgyebMezok().put("kategoriaNev", (kat!=null) ? KategoriaCache.getKategoriaNevChain(hirdetes.getKategoriaId()) : "");
-			hirdetes.getEgyebMezok().put("kategoriaUrlNev", (kat!=null) ? kat.getUrlNev() : "");
-			
-			Helyseg hely = HelysegCache.getCacheById().get(hirdetes.getHelysegId());
-			hirdetes.getEgyebMezok().put("helysegNev", (hely!=null) ? HelysegCache.getHelysegNevChain(hirdetes.getHelysegId()) : "");
-			hirdetes.getEgyebMezok().put("helysegUrlNev", (hely!=null) ? hely.getUrlNev() : "");
-			
-			hirdetes.getEgyebMezok().put("feladvaSzoveg", new SimpleDateFormat("yyyy. MMMM d. EEEE", new Locale("hu")).format(hirdetes.getFeladasDatuma()));
-			
-			// Attributumok cimenek, ertekenek (ha kell) atirasa
-			LinkedList<Attributum> attributumList = AttributumCache.getKATEGORIA_ATTRIBUTUM().get(kat.getUrlNev());
-			HashMap<String, Object> attributumok = new HashMap<String, Object>();
-			for(String key : hirdetes.getAttributumok().keySet()) {
-				for(Attributum attr : attributumList) {
-					if(attr.getNev().equalsIgnoreCase(key)) {
-						Object o = hirdetes.getAttributumok().get(key);
-						// Ha van mertekegyseg, az ertek moge irjuk 
-						if(attr.getMertekEgyseg() != null) {
-							o = new String(o.toString() + " " + attr.getMertekEgyseg());
+			if(hirdetes.isTorolve()) {
+				// Letezik a hirdetes, de mar lejart es torolve lett
+				dataModel.put("hibaUzenet", "A megadott hirdetés már lejárt, és feladója nem hosszabbította meg.");
+			} else {
+				// HTML Title modositasa
+				((HashMap<String, Object>) dataModel.get("app")).put("htmlTitle", getApplication().getName() + " - " + hirdetes.getCim());
+				((HashMap<String, Object>) dataModel.get("app")).put("description", hirdetes.getCim());
+				
+				// Mongo Datastore
+				Datastore datastore = MongoUtils.getDatastore();
+				
+				// Megjelenes szamanak novelese
+				Query<Hirdetes> query = datastore.createQuery(Hirdetes.class);
+				query.criteria("id").equal(this.hirdetesId);
+				UpdateOperations<Hirdetes> ops = datastore.createUpdateOperations(Hirdetes.class).set("megjelenes", hirdetes.getMegjelenes()+1);
+				datastore.update(query, ops);
+			    hirdetes.increaseMegjelenesByOne();
+			    
+				
+				// Kereses eredmenyeben levo Hirdetes objektum feltoltese kepekkel, egyeb adatokkal a megjeleniteshez
+				hirdetes.getEgyebMezok().put("tipusNev", (hirdetes.getTipus()==HirdetesTipus.KINAL) ? "Kínál" : "Keres");
+				
+				Kategoria kat = KategoriaCache.getCacheById().get(hirdetes.getKategoriaId());
+				hirdetes.getEgyebMezok().put("kategoriaNev", (kat!=null) ? KategoriaCache.getKategoriaNevChain(hirdetes.getKategoriaId()) : "");
+				hirdetes.getEgyebMezok().put("kategoriaUrlNev", (kat!=null) ? kat.getUrlNev() : "");
+				
+				Helyseg hely = HelysegCache.getCacheById().get(hirdetes.getHelysegId());
+				hirdetes.getEgyebMezok().put("helysegNev", (hely!=null) ? HelysegCache.getHelysegNevChain(hirdetes.getHelysegId()) : "");
+				hirdetes.getEgyebMezok().put("helysegUrlNev", (hely!=null) ? hely.getUrlNev() : "");
+				
+				hirdetes.getEgyebMezok().put("feladvaSzoveg", new SimpleDateFormat("yyyy. MMMM d. EEEE", new Locale("hu")).format(hirdetes.getFeladasDatuma()));
+				
+				// Attributumok cimenek, ertekenek (ha kell) atirasa
+				LinkedList<Attributum> attributumList = AttributumCache.getKATEGORIA_ATTRIBUTUM().get(kat.getUrlNev());
+				HashMap<String, Object> attributumok = new HashMap<String, Object>();
+				for(String key : hirdetes.getAttributumok().keySet()) {
+					for(Attributum attr : attributumList) {
+						if(attr.getNev().equalsIgnoreCase(key)) {
+							Object o = hirdetes.getAttributumok().get(key);
+							// Ha van mertekegyseg, az ertek moge irjuk 
+							if(attr.getMertekEgyseg() != null) {
+								o = new String(o.toString() + " " + attr.getMertekEgyseg());
+							}
+							// Ha az ertek boolean, leforditjuk
+							if(o instanceof Boolean) {
+								o = ((Boolean) o) ? new String("igen") : new String("nem");
+							}
+							// Ha van ertekMap, kiszedjuk az ertekhez tartozo nevet
+							if(attr.getErtekMap() != null) {
+								o = attr.getErtekMap().get(o.toString());
+							}
+							attributumok.put(attr.getCim(), o);
+							break;
 						}
-						// Ha az ertek boolean, leforditjuk
-						if(o instanceof Boolean) {
-							o = ((Boolean) o) ? new String("igen") : new String("nem");
-						}
-						// Ha van ertekMap, kiszedjuk az ertekhez tartozo nevet
-						if(attr.getErtekMap() != null) {
-							o = attr.getErtekMap().get(o.toString());
-						}
-						attributumok.put(attr.getCim(), o);
-						break;
 					}
 				}
+				hirdetes.setAttributumok(attributumok);
+				
+				// Kepek
+				Query<HirdetesKep> kepekQuery = datastore.createQuery(HirdetesKep.class);
+				kepekQuery.criteria("hirdetesId").equal(hirdetes.getId());
+				
+				for(HirdetesKep kep : kepekQuery) {
+					hirdetes.getKepek().add(kep);
+				}
+				
+				dataModel.put("hirdetes", hirdetes);
 			}
-			hirdetes.setAttributumok(attributumok);
-			
-			// Kepek
-			Query<HirdetesKep> kepekQuery = datastore.createQuery(HirdetesKep.class);
-			kepekQuery.criteria("hirdetesId").equal(hirdetes.getId());
-			
-			for(HirdetesKep kep : kepekQuery) {
-				hirdetes.getKepek().add(kep);
-			}
-			
-			dataModel.put("hirdetes", hirdetes);
 		} else {
 			// Nincs ilyen hirdetes
 			getLogger().severe("Nincs meg a megadott hirdetes: " + (String) this.getRequestAttributes().get("hirdetesId"));
